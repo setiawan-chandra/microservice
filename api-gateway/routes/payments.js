129707c384fcb9552be8df6f671bfbfc29c2d5d2
Setiawan Chandra <wawatan17@gmail.com>
2023-09-05 19:48:31 +0700

    Feat: Set up API Gateway Part 2 - Install Depedencies dotenv (npm install dotenv) - Install Depedencies axios (npm install axios --save) - Create routes - Create .env

--- api-gateway/routes/payments.js
+++ api-gateway/routes/payments.js
@@ -4,7 +4,7 @@ const { APP_NAME } = process.env;
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
-  res.send(APP_NAME);
+  res.send("payments");
 });
 
 module.exports = router;
