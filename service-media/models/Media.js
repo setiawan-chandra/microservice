1a1d1e2dd52aeeac101e917abeb3bc589276e982
Setiawan Chandra <wawatan17@gmail.com>
2023-09-06 15:18:24 +0700

    Feat: Create Migration and model for Media - install mysql2 package (npm install mysql2)

---
+++ service-media/models/Media.js
@@ -0,0 +1,31 @@
+module.export = (sequelize, DataTypes) => {
+  const Media = sequelize.define(
+    "Media",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      image: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        field: "created_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      updatedAt: {
+        field: "updated_at",
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+    },
+    {
+      tableName: "media",
+    }
+  );
+  return Media;
+};
