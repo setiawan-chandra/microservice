29682f87067c83cdcb667ebda2d2931fb2823c3c
Setiawan Chandra <wawatan17@gmail.com>
2023-09-09 18:23:37 +0700

    Feat: Integrasi API Gateway GET (2)

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -5,5 +5,6 @@ const mediaHandler = require("./handler/media");
 
 /* GET users listing. */
 router.post("/", mediaHandler.create);
+router.get("/", mediaHandler.getAll);
 
 module.exports = router;
