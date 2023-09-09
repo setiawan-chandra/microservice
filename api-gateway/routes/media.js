a1278a31988d8a6a763ac7fa271f7a9439d6c5e7
Setiawan Chandra <wawatan17@gmail.com>
2023-09-09 19:03:49 +0700

    Feat: Integrasi API Gateway Delete (3)

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -6,5 +6,6 @@ const mediaHandler = require("./handler/media");
 /* GET users listing. */
 router.post("/", mediaHandler.create);
 router.get("/", mediaHandler.getAll);
+router.delete("/:id", mediaHandler.destroy);
 
 module.exports = router;
