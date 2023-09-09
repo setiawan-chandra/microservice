08cc8b02b96ccced74d3fb70943fde5beb760c2f
Setiawan Chandra <wawatan17@gmail.com>
2023-09-09 18:19:21 +0700

    Feat: Integrasi API Gateway 1

--- api-gateway/routes/media.js
+++ api-gateway/routes/media.js
@@ -1,10 +1,9 @@
-var express = require("express");
-var router = express.Router();
-const { APP_NAME } = process.env;
+const express = require("express");
+const router = express.Router();
+
+const mediaHandler = require("./handler/media");
 
 /* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("media");
-});
+router.post("/", mediaHandler.create);
 
 module.exports = router;
