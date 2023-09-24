1a75e16cb842122a4cc9b391ac0c862533666c8f
Setiawan Chandra <wawatan17@gmail.com>
2023-09-24 21:33:13 +0700

    Feat: Migration Table Refresh Tokens

--- service-user/migrations/20230924132343-create-table-refresh-tokens.js
+++ service-user/migrations/20230924132343-create-table-refresh-tokens.js
@@ -1,22 +1,45 @@
-'use strict';
+"use strict";
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("refresh_tokens", {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false,
+      },
+      token: {
+        type: Sequelize.TEXT,
+        allowNull: false,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+        allowNull: false,
+      },
+    });
+
+    await queryInterface.addConstraint("refresh_tokens", {
+      type: "foreign key",
+      name: "REFRESH_TOKENS_USER_ID",
+      fields: ["user_id"],
+      references: {
+        table: "users",
+        field: "id",
+      },
+    });
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("refresh_tokens");
+  },
 };
